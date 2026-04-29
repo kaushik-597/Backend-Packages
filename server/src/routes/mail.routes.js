c4670960292964ce3a8135e5d4605a5473fab886
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-04-29 11:49:29 +0530

    feat: apply rate limiter middleware to mail route to restrict excessive requests

--- server/src/routes/mail.routes.js
+++ server/src/routes/mail.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { sendTestMail } from "../controllers/mail.controllers.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { mailSchema } from "../validators/mail.validators.js";
+import { mailRateLimiter } from "../middlewares/mailRateLimiter.js";
 
 const router = Router();
 
-router.route("/send").post(validate(mailSchema), sendTestMail);
+router.route("/send").post(mailRateLimiter, validate(mailSchema), sendTestMail);
 
 export default router;
