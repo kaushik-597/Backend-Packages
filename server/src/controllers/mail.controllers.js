4de9bd2a993866be972f9fae3836665491869e21
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-05-04 17:13:35 +0530

    feat: implement mail controllers for fetching all mails and deleting mail by id with proper error handling

--- server/src/controllers/mail.controllers.js
+++ server/src/controllers/mail.controllers.js
@@ -1,4 +1,5 @@
 import { Mail } from "../models/mails.model.js";
+import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { sendMail } from "../utils/mailer.js";
@@ -36,4 +37,24 @@ export const sendTestMail = asyncHandler(async (req, res) => {
       .status(500)
       .json(new ApiResponse(500, mailLog, "Mail sending failed"));
   }
+});
+
+export const getMails = asyncHandler(async (req, res) => {
+  const mails = await Mail.find().sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, mails, "Mails fetched successfully"));
+});
+
+export const deleteMail = asyncHandler(async (req, res) => {
+  const mail = await Mail.findByIdAndDelete(req.params.id);
+
+  if (!mail) {
+    throw new ApiError(404, "Mail not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, mail, "Mail deleted successfully"));
 });
